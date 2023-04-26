53a0dca6dab3cf5a566373a850a90b2679e10124
Oryatory <larin.ia28@gmail.com>
2023-04-27 03:26:39 +0400

    font-fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./scss/index.scss";
-import "/src/assets/fonts/SteinbeckRegular.otf";
+import "/src/assets/fonts/SteinbeckRegular.woff";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
