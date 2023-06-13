1b678fa703f315616241b64d7bbc2ef700909860
Oryatory <larin.ia28@gmail.com>
2023-06-13 06:37:00 +0400

    added background noise

--- src/Background.jsx
+++ src/Background.jsx
@@ -2,15 +2,17 @@ const Background = ({ theme }) => {
   return (
     <div className="background-container">
       <div
-        className="background"
+        className="background__image"
         style={{
-          // background:
-          //   theme === "light-theme"
-          //     ? "url(/src/assets/images/light-background.png)"
-          //     : "url(/src/assets/images/dark-background.png)",
           filter: theme === "light-theme" ? "none" : "invert(100%)",
           opacity: theme === "light-theme" ? "33%" : "100%",
         }}
+      ></div>
+      <div
+        className="background__noise"
+        // style={{
+        //   filter: theme === "light-theme" ? "none" : "invert(100%)",
+        // }}
       ></div>
     </div>
   );
